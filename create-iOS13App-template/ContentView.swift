d55767716098dd7ae2071427b469f08c89802aa2
Gibbs3699 <gift_kamonpan@hotmail.com>
2021-12-10 21:23:51 +0700

    Initial Commit

---
+++ create-iOS13App-template/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  create-iOS13App-template
+//
+//  Created by admin on 10/12/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

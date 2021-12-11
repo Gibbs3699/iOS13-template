c8e7642e84be357723d2f2be58749b59244ddd5a
Gibbs3699 <gift_kamonpan@hotmail.com>
2021-12-11 17:07:17 +0700

    Adding color literal, gradient and overlay

--- create-iOS13App-template/MenuView.swift
+++ create-iOS13App-template/MenuView.swift
@@ -13,16 +13,36 @@ struct MenuView: View {
             Spacer()
             
             VStack(spacing: 16) {
+                
+                Text("TheGIZzz - 28% complete")
+                    .font(.caption)
+                Color.white
+                    .frame(width: 38, height: 6)
+                    .cornerRadius(3)
+                    .frame(width: 130, height: 6, alignment: .leading)
+                    .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)).opacity(0.6))
+                    .cornerRadius(3)
+                    .padding()
+                    .frame(width: 150, height: 24)
+                    .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)).opacity(0.2))
+                    .cornerRadius(12)
+                
                 MenuRow(title: "Account", icon: "gear")
                 MenuRow(title: "Billing", icon: "creditcard")
                 MenuRow(title: "Sign out", icon: "person.crop.circle")
             }
             .frame(maxWidth: .infinity)
             .frame(height: 300)
-            .background(Color.white)
+            .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.8084815362, green: 0.7902384962, blue: 0.9718133802, alpha: 1))]), startPoint: .top, endPoint: .bottom))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(radius: 30)
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20.0)
             .padding(.horizontal, 30)
+            .overlay(Image("60")
+                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                        .frame(width: 60, height: 60)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        .offset(y: -150)
+            )
         }
         .padding(.bottom, 30)
     }
@@ -44,6 +64,7 @@ struct MenuRow: View {
                 .font(.system(size: 20, weight: .light))
                 .imageScale(.large)
                 .frame(width: 32, height: 32)
+                .foregroundColor(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
             
             Text(title)
                 .font(.system(size: 20, weight: .bold, design: .rounded))

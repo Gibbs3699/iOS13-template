d898709db461279dbd2b070a0968a3d94d7870a0
Gibbs3699 <gift_kamonpan@hotmail.com>
2021-12-12 16:24:18 +0700

    Adding navigation view and list

---
+++ create-iOS13App-template/UpdateList.swift
@@ -0,0 +1,67 @@
+//
+//  UpdateList.swift
+//  create-iOS13App-template
+//
+//  Created by admin on 12/12/2564 BE.
+//
+
+import SwiftUI
+
+struct UpdateList: View {
+    var body: some View {
+        NavigationView {
+            List(updateData) { update in
+                NavigationLink(destination: Text(update.text)) {
+                    HStack {
+                        Image(update.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 80, height: 80)
+                            .background(Color.black)
+                            .cornerRadius(20)
+                            .padding(.trailing, 4)
+                        
+                        VStack(alignment: .leading, spacing: 8.0) {
+                            Text(update.title)
+                                .font(.system(size: 20, weight: .bold))
+                            
+                            Text(update.text)
+                                .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                                .font(.subheadline)
+                                .foregroundColor(Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)))
+                            
+                            Text(update.date)
+                                .font(.caption)
+                                .fontWeight(.bold)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                    .padding(.vertical, 8)
+                }
+            }
+            .navigationTitle(Text("Updates"))
+        }
+    }
+}
+
+struct UpdateList_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateList()
+    }
+}
+
+struct Update: Identifiable {
+    var id = UUID()
+    var image : String
+    var title : String
+    var text : String
+    var date : String
+}
+
+let updateData = [
+    Update(image: "Card1", title: "SwiftUI Advanced", text: "Text", date: "Jan 1"),
+    Update(image: "Card2", title: "Website", text: "Text", date: "Jan 1"),
+    Update(image: "Card3", title: "ProtoPie", text: "Text", date: "Jan 1"),
+    Update(image: "Card4", title: "SwiftUI", text: "Text", date: "Jan 1"),
+    Update(image: "Card5", title: "Framer Playground", text: "Text", date: "Jan 1")
+]

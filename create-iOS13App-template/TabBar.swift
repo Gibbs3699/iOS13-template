85fb413b06809d9001c7eb118bcdeae9884a9057
Gibbs3699 <gift_kamonpan@hotmail.com>
2021-12-12 17:12:57 +0700

    Adding tab bar and test on device

---
+++ create-iOS13App-template/TabBar.swift
@@ -0,0 +1,31 @@
+//
+//  TabBar.swift
+//  create-iOS13App-template
+//
+//  Created by admin on 12/12/2564 BE.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            Home().tabItem {
+                Image(systemName: "play.circle.fill")
+                Text("Home")
+            }
+            
+            ContentView().tabItem {
+                Image(systemName: "rectangle.stack.fill")
+                Text("Certificates")
+            }
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}

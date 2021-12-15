b1ff9845f45d5bdd7ee4f621c4ed8f984ad62064
Gibbs3699 <gift_kamonpan@hotmail.com>
2021-12-15 23:59:18 +0700

    Adding css to stacked cards animation

--- create-iOS13App-template/CourseList.swift
+++ create-iOS13App-template/CourseList.swift
@@ -17,10 +17,16 @@ struct CourseList: View {
                 Text("content 2").font(.title).bold()
                 Text("content 2")
             }
-            
+            .padding(20)
+            .frame(maxWidth: show ? .infinity : screen.width-60, maxHeight: show ? .infinity : 280, alignment: .top)
+            .offset(y: show ? 480:0)
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)).opacity(0.3), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
+            .opacity(show ? 1:0)
             
             VStack {
-                CourseView()
+                CourseView(show: $show)
                 Spacer()
                 Image(uiImage: #imageLiteral(resourceName: "Card2"))
                     .resizable()
@@ -31,16 +37,16 @@ struct CourseList: View {
             .padding(show ? 30 : 20)
             .padding(.top, show ? 30 : 0)
     //        .frame(width: show ? screen.width : screen.width - 60, height: show ? screen.height : 280)
-            .frame(maxWidth: show ? .infinity : screen.width-60, maxHeight: show ? .infinity : 280)
+            .frame(maxWidth: show ? .infinity : screen.width-60, maxHeight: show ? 460 : 280)
             .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)).opacity(0.3), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
-            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
             .onTapGesture {
                 self.show.toggle()
             }
-            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
         }
+        .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
@@ -51,6 +57,8 @@ struct CourseList_Previews: PreviewProvider {
 }
 
 struct CourseView: View {
+    @Binding var show : Bool
+    
     var body: some View {
         VStack {
             HStack(alignment: .top) {
@@ -62,7 +70,18 @@ struct CourseView: View {
                         .foregroundColor(.white.opacity(0.7))
                 }
                 Spacer()
-                Image(uiImage: #imageLiteral(resourceName: "Logo1"))
+                ZStack {
+                    Image(uiImage: #imageLiteral(resourceName: "Logo1"))
+                        .opacity(show ? 1:0)
+                    
+                    
+                    VStack {
+                        Image(systemName: "xmark")
+                            .font(.system(size: 16, weight: .medium))
+                            .foregroundColor(.white)
+                    }
+                }
+                
             }
         }
     }

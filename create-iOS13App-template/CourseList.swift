7975ab8ed9ea1c1fe8796511699545224f2cdb05
Gibbs3699 <gift_kamonpan@hotmail.com>
2021-12-16 00:22:16 +0700

    Revise stacked cards animation

--- create-iOS13App-template/CourseList.swift
+++ create-iOS13App-template/CourseList.swift
@@ -1,52 +1,18 @@
 //
 //  CourseList.swift
-//  create-iOS13App-template
+//  DesignCode
 //
-//  Created by admin on 15/12/2564 BE.
+//  Created by Meng To on 2020-01-07.
+//  Copyright © 2020 Meng To. All rights reserved.
 //
 
 import SwiftUI
 
 struct CourseList: View {
-    @State var show = false
-    
     var body: some View {
-        ZStack(alignment: .top) {
-            VStack(alignment: .leading, spacing: 30.0) {
-                Text("content 1")
-                Text("content 2").font(.title).bold()
-                Text("content 2")
-            }
-            .padding(20)
-            .frame(maxWidth: show ? .infinity : screen.width-60, maxHeight: show ? .infinity : 280, alignment: .top)
-            .offset(y: show ? 480:0)
-            .background(Color.white)
-            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(color: Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)).opacity(0.3), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
-            .opacity(show ? 1:0)
-            
-            VStack {
-                CourseView(show: $show)
-                Spacer()
-                Image(uiImage: #imageLiteral(resourceName: "Card2"))
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 140, alignment: .top)
-            }
-            .padding(show ? 30 : 20)
-            .padding(.top, show ? 30 : 0)
-    //        .frame(width: show ? screen.width : screen.width - 60, height: show ? screen.height : 280)
-            .frame(maxWidth: show ? .infinity : screen.width-60, maxHeight: show ? 460 : 280)
-            .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
-            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(color: Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)).opacity(0.3), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
-            .onTapGesture {
-                self.show.toggle()
-            }
+        VStack {
+            CourseView()
         }
-        .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
 }
 
@@ -57,36 +23,73 @@ struct CourseList_Previews: PreviewProvider {
 }
 
 struct CourseView: View {
-    @Binding var show : Bool
+    @State var show = false
     
     var body: some View {
-        VStack {
-            HStack(alignment: .top) {
-                VStack(alignment: .leading, spacing: 8.0) {
-                    Text("SwiftUI Advanced")
-                        .font(.system(size: 24, weight: .bold))
-                        .foregroundColor(.white)
-                    Text("SwiftUI Advanced")
-                        .foregroundColor(.white.opacity(0.7))
+        ZStack(alignment: .top) {
+            VStack(alignment: .leading, spacing: 30.0) {
+                Text("Take your SwiftUI app to the App Store with advanced techniques like API data, packages and CMS.")
+                
+                Text("About this course")
+                    .font(.title).bold()
+                
+                Text("This course is unlike any other. We care about design and want to make sure that you get better at it in the process. It was written for designers and developers who are passionate about collaborating and building real apps for iOS and macOS. While it's not one codebase for all apps, you learn once and can apply the techniques and controls to all platforms with incredible quality, consistency and performance. It's beginner-friendly, but it's also packed with design tricks and efficient workflows for building great user interfaces and interactions.")
+                
+                Text("Minimal coding experience required, such as in HTML and CSS. Please note that Xcode 11 and Catalina are essential. Once you get everything installed, it'll get a lot friendlier! I added a bunch of troubleshoots at the end of this page to help you navigate the issues you might encounter.")
+            }
+            .padding(30)
+            .frame(maxWidth: CGFloat(show ? .infinity : screen.width - 60), maxHeight: CGFloat(show ? .infinity : 280.0), alignment: .top)
+            .offset(y: show ? 460 : 0)
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+            .opacity(show ? 1 : 0)
+            
+            VStack {
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading, spacing: 8.0) {
+                        Text("SwiftUI Advanced")
+                            .font(.system(size: 24, weight: .bold))
+                            .foregroundColor(.white)
+                        Text("20 Sections")
+                            .foregroundColor(Color.white.opacity(0.7))
+                    }
+                    Spacer()
+                    ZStack {
+                        Image(uiImage: #imageLiteral(resourceName: "Logo1"))
+                            .opacity(show ? 0 : 1)
+                        
+                        VStack {
+                            Image(systemName: "xmark")
+                                .font(.system(size: 16, weight: .medium))
+                                .foregroundColor(.white)
+                        }
+                        .frame(width: 36, height: 36)
+                        .background(Color.black)
+                        .clipShape(Circle())
+                        .opacity(show ? 1 : 0)
+                    }
                 }
                 Spacer()
-                ZStack {
-                    Image(uiImage: #imageLiteral(resourceName: "Logo1"))
-                        .opacity(show ? 0:1)
-                    
-                    
-                    VStack {
-                        Image(systemName: "xmark")
-                            .font(.system(size: 16, weight: .medium))
-                            .foregroundColor(.white)
-                    }
-                    .frame(width: 36, height: 36)
-                    .background(Color.black)
-                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                    .opacity(show ? 1:0)
-                }
-                
+                Image(uiImage: #imageLiteral(resourceName: "Card2"))
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 140, alignment: .top)
+            }
+            .padding(show ? 30 : 20)
+            .padding(.top, CGFloat(show ? 30 : 0))
+    //        .frame(width: show ? screen.width : screen.width - 60, height: show ? screen.height : 280)
+            .frame(maxWidth: CGFloat(show ? .infinity : screen.width - 60), maxHeight: CGFloat(show ? 460 : 280))
+            .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)).opacity(0.3), radius: 20, x: 0, y: 20)
+            
+            .onTapGesture {
+                self.show.toggle()
             }
         }
+        .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+        .edgesIgnoringSafeArea(.all)
     }
 }

be93885cdb50231ea114a3f6c14114a73865d8cc
Gibbs3699 <gift_kamonpan@hotmail.com>
2021-12-15 23:44:16 +0700

    Adding custom transition

---
+++ create-iOS13App-template/CourseList.swift
@@ -0,0 +1,60 @@
+//
+//  CourseList.swift
+//  create-iOS13App-template
+//
+//  Created by admin on 15/12/2564 BE.
+//
+
+import SwiftUI
+
+struct CourseList: View {
+    @State var show = false
+    
+    var body: some View {
+        VStack {
+            CourseView()
+            Spacer()
+            Image(uiImage: #imageLiteral(resourceName: "Card2"))
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: .infinity)
+                .frame(height: 140, alignment: .top)
+        }
+        .padding(show ? 30 : 20)
+        .padding(.top, show ? 30 : 0)
+//        .frame(width: show ? screen.width : screen.width - 60, height: show ? screen.height : 280)
+        .frame(maxWidth: show ? .infinity : screen.width-60, maxHeight: show ? .infinity : 280)
+        .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+        .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .shadow(color: Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)).opacity(0.3), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
+        .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+        .onTapGesture {
+            self.show.toggle()
+        }
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CourseList_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseList()
+    }
+}
+
+struct CourseView: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                VStack(alignment: .leading, spacing: 8.0) {
+                    Text("SwiftUI Advanced")
+                        .font(.system(size: 24, weight: .bold))
+                        .foregroundColor(.white)
+                    Text("SwiftUI Advanced")
+                        .foregroundColor(.white.opacity(0.7))
+                }
+                Spacer()
+                Image(uiImage: #imageLiteral(resourceName: "Logo1"))
+            }
+        }
+    }
+}

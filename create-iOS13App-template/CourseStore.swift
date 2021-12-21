a641cafc84f649854349e5a04453be39fa689789
Gibbs3699 <gift_kamonpan@hotmail.com>
2021-12-21 22:26:10 +0700

    Adding contentful and combine

--- create-iOS13App-template/CourseStore.swift
+++ create-iOS13App-template/CourseStore.swift
@@ -10,10 +10,34 @@ import Contentful
 
 let client = Client(spaceId: "0ge8xzmnbp2c", accessToken: "03010b0d79abcb655ca3fda453f2f493b5472e0aaa53664bc7dea5ef4fce2676")
 
-func getArray() {
-    let query = Query.where(contentTypeId: "course")
+func getArray(id: String, completion: @escaping([Entry]) -> ()) {
+    let query = Query.where(contentTypeId: id)
     
     client.fetchArray(of: Entry.self, matching: query) { result in
-        print(result)
+        switch result{
+        case .success(let array):
+            DispatchQueue.main.async {
+                completion(array.items)
+            }
+        case .failure(let error):
+            print(error)
+        }
+    }
+}
+
+class CourseStore: ObservableObject {
+    @Published var courses: [Course] = courseData
+    
+    init() {
+        getArray(id: "course") { (items) in
+            items.forEach { (item) in
+                self.courses.append(Course(title: item.fields["title"] as! String,
+                    subtitle: item.fields["subtitle"] as! String,
+                    image: #imageLiteral(resourceName: "Background1"),
+                    logo: #imageLiteral(resourceName: "Logo1"),
+                    color: #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1),
+                    show: false))
+            }
+        }
     }
 }

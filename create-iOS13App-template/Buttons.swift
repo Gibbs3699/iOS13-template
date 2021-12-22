c4ca1b50975517f874955c068b05817671c15cdd
Gibbs3699 <gift_kamonpan@hotmail.com>
2021-12-22 23:58:35 +0700

    Adding haptic feedback

--- create-iOS13App-template/Buttons.swift
+++ create-iOS13App-template/Buttons.swift
@@ -7,6 +7,14 @@
 
 import SwiftUI
 
+func haptic(type: UINotificationFeedbackGenerator.FeedbackType) {
+    UINotificationFeedbackGenerator().notificationOccurred(.success)
+}
+
+func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+    UIImpactFeedbackGenerator(style: style).impactOccurred()
+}
+
 struct Buttons: View {
     var body: some View {
         VStack(spacing: 50) {
@@ -75,6 +83,7 @@ struct RectangleButton: View {
             .gesture(
                 LongPressGesture(minimumDuration: 0.5, maximumDistance: 10).onChanged { value in
                     self.tap = true
+                    impact(type: .heavy)
                     DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                         self.tap = false
                     }

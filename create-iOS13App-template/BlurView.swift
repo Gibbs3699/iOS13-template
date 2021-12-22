e33374fc54c37c5569497ac53dc1a3bb88d7fbf1
“Gibbs3699” <gift_kamonpan@hotmail.com>
2021-12-22 20:41:27 +0700

    Revise background blur

--- create-iOS13App-template/BlurView.swift
+++ create-iOS13App-template/BlurView.swift
@@ -11,23 +11,24 @@ struct BlurView: UIViewRepresentable {
     typealias UIViewType = UIView
     var style: UIBlurEffect.Style
     
-    func makeUIView(context: Context) -> UIView {
+    func makeUIView(context: UIViewRepresentableContext<BlurView>) -> UIView {
         let view = UIView(frame: CGRect.zero)
         view.backgroundColor = .clear
         
-        let blurEffect = UIBlurEffect(style: .systemMaterial)
+        let blurEffect = UIBlurEffect(style: style)
         let blurView = UIVisualEffectView(effect: blurEffect)
         blurView.translatesAutoresizingMaskIntoConstraints = false
         view.insertSubview(blurView, at: 0)
         
-        NSLayoutConstraint
-            .activate([blurView.widthAnchor.constraint(equalTo: view.widthAnchor), blurView.heightAnchor.constraint(equalTo: view.widthAnchor)])
-                      
+        NSLayoutConstraint.activate([
+            blurView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            blurView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+        
         return view
     }
     
-    func updateUIView(_ uiView: UIView, context: Context) {
-//
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<BlurView>) {
+        
     }
-
 }

cafd77fd88b4067baa94a989516c1b17e43b8e8f
Gibbs3699 <gift_kamonpan@hotmail.com>
2021-12-23 20:46:09 +0700

    Adding 3d parallax and looping animation

--- create-iOS13App-template/LoginView.swift
+++ create-iOS13App-template/LoginView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var show = false
+    @State var viewState = CGSize.zero
+    @State var isDragging = false
+    
     var body: some View {
         ZStack(alignment: .top) {
             Color.black.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
@@ -21,6 +25,7 @@ struct LoginView: View {
                 }
                 .frame(maxWidth: 375, maxHeight:  100)
                 .padding(.horizontal, 16)
+                .offset(x: viewState.width/15, y: viewState.height/15)
                 .multilineTextAlignment(.center)
                 
                 Text("80 hours of courses for SwiftUI")
@@ -37,17 +42,40 @@ struct LoginView: View {
                 ZStack {
                     Image(uiImage: #imageLiteral(resourceName: "Blob"))
                         .offset(x: -150, y: -200)
+                        .rotationEffect(Angle(degrees: show ? 360+90 : 90))
                         .blendMode(.plusDarker)
+                        .animation(Animation.linear(duration: 120).repeatForever(autoreverses: false))
+                        .onAppear { self.show = true }
+
                     
                     Image(uiImage: #imageLiteral(resourceName: "Blob"))
                         .offset(x: -200, y: -250)
+                        .rotationEffect(Angle(degrees: show ? 360 : 0), anchor: .leading)
                         .blendMode(.overlay)
+                        .animation(Animation.linear(duration: 120).repeatForever(autoreverses: false))
                 }
                 
             )
-            .background(Image(uiImage: #imageLiteral(resourceName: "Card3")), alignment: .bottom)
+            .background(
+                Image(uiImage: #imageLiteral(resourceName: "Card3"))
+                    .offset(x: viewState.width/15, y: viewState.height/15)
+                , alignment: .bottom)
             .background(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .scaleEffect(isDragging ? 0.9 : 1)
+            .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration:  0.8))
+            .rotation3DEffect(
+                Angle(degrees: 5),
+                axis: (x: viewState.width, y: viewState.height, z: 0.0)
+            )
+            .gesture(
+                DragGesture().onChanged { value in
+                    self.viewState = value.translation
+                }
+                .onEnded { value in
+                    self.viewState = .zero
+                }
+            )
         }
         
     }
